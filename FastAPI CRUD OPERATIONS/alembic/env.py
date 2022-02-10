b7af0afda22c4dfe990d0b7803c1d76ef2efbc48
Luka Topalovic <luka.topalovic@thingsolver.com>
2022-02-10 10:36:37 +0100

    test

--- FastAPI CRUD OPERATIONS/alembic/env.py
+++ FastAPI CRUD OPERATIONS/alembic/env.py
@@ -1,7 +1,11 @@
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy     import pool
+
+
+
+
 
 from alembic import context
 
@@ -25,7 +29,7 @@ target_metadata = None
 # ... etc.
 
 
-def run_migrations_offline():
+def run_migrations_offline(                     ):
     """Run migrations in 'offline' mode.
 
     This configures the context with just a URL
@@ -49,7 +53,7 @@ def run_migrations_offline():
         context.run_migrations()
 
 
-def run_migrations_online():
+def run_migrations_online(                      ):
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
@@ -60,9 +64,9 @@ def run_migrations_online():
         config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
-    )
+                                    )
 
-    with connectable.connect() as connection:
+    with connectable.connect(                       ) as connection:
         context.configure(
             connection=connection, target_metadata=target_metadata
         )
